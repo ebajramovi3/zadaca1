b6c746334c415dc3db32a324b52e797de5fb2120
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2022-11-18 22:41:47 +0100

    Modifications

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,34 +1,25 @@
 package ba.unsa.etf.rpr;
 
-import java.sql.SQLOutput;
-
 /**
- * Hello world!
- *
+ * Main program
+ * converts command line input to one string separated with blank spaces
  */
-public class App 
+
+public class App
 {
     public static void main( String[] args ){
-        if (args.length < 2){
-            System.err.println("Nedovoljan broj argumenata java -jar rpr-zadaca1.jar [funkcija] [argument]");
-        } else if (!args[0].equals("evaluate")) {
-            System.err.println("Nevalidna funkcija, evaluate je podrzana funkcija");
-        }else{
-        String input = new String();
-        for(int i = 0; i < args.length; i++){
-            if(i >= 2)
-            input = input + " " + args[i];
-            else if(i == 1)
-                input = args[i];
+        StringBuilder input = new StringBuilder();
+        for(int i = 0; i < args.length; i++) {
+            if (i >= 1)
+                input.append(" ").append(args[i].trim());
+            else
+                input.append(args[i].trim());
         }
-            System.out.println(input);
-        ExpressionEvaluator result = new ExpressionEvaluator();
         try{
-            System.out.println(result.evaluate(input));
+            System.out.println((ExpressionEvaluator.evaluate(input.toString())).toString());
         }catch(Exception exception){
             System.out.println(exception.getMessage());
         }
-        }
 
-}
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -3,39 +3,59 @@ package ba.unsa.etf.rpr;
 import java.util.Stack;
 import static java.lang.Double.parseDouble;
 
+/**
+ * class ExpressionEvaluator
+ * two private attributes
+ * Dijkstra’s Algorithm
+ */
 public class ExpressionEvaluator {
-    private static java.util.Stack<String> operators = new Stack<String>();
-    private static java.util.Stack<Double> values = new Stack<Double>();
+    private static final java.util.Stack<String> operators = new Stack<>();
+    private static final java.util.Stack<Double> values = new Stack<>();
 
     public static Double evaluate(String str) throws RuntimeException {
-        String[] izraz = str.split(" ");
-        Double result = (double) 0;
-        for(int i = 0; i < izraz.length; i++){
-            if(izraz[i].equals( "*") || izraz[i].equals("/") || izraz[i].equals("+") || izraz[i].equals("-") || izraz[i].equals("sqrt"))
-                 operators.push(izraz[i]);
-            else if(izraz[i].equals(")")) {
-                result = values.pop();
-                String operator = operators.pop();
-                    if(operator.equals("*"))
-                        result = result * values.pop();
-                    else if(operator.equals("/"))
-                        result = values.pop() / result;
-                    else if(operator.equals("+"))
-                        result = result + values.pop();
-                    else if(operator.equals("-"))
-                        result = values.pop() - result;
-                    else if(operator.equals("sqrt"))
-                        result = Math.sqrt(result);
-                    values.push(result);
-            }
-            else if(!izraz[i].equals("("))
+        String[] expression = str.split(" ");
+        Double result;
+        for(int i = 0; i < expression.length; i++) {
+            if (expression[i].equals("*") || expression[i].equals("/") || expression[i].equals("+") || expression[i].equals("-") || expression[i].equals("sqrt"))
+                operators.push(expression[i]);
+            else if (expression[i].equals(")")) {
+                String operator;
                 try {
-                    values.push(parseDouble(izraz[i]));
-                }catch(NumberFormatException exception){
+                    result = values.pop();
+                    operator = operators.pop();
+                } catch (Exception exception) {
+                    throw new RuntimeException("Izraz nije validan");
+                }
+                switch (operator) {
+                    case "*":
+                        result = result * values.pop();
+                        break;
+                    case "/":
+                        result = values.pop() / result;
+                        break;
+                    case "+":
+                        result = result + values.pop();
+                        break;
+                    case "-":
+                        result = values.pop() - result;
+                        break;
+                    case "sqrt":
+                        result = Math.sqrt(result);
+                        break;
+                }
+                values.push(result);
+            } else if (!expression[i].equals("(")) {
+                if (i == 0)
+                    throw new RuntimeException("Izraz nije validan");
+
+                try {
+                    values.push(parseDouble(expression[i]));
+                } catch (NumberFormatException exception) {
                     throw new RuntimeException("Izraz nije validan");
                 }
 
             }
+        }
         if(operators.size() != 0) throw new RuntimeException("Izraz nije validan");
         return values.pop();
     }
